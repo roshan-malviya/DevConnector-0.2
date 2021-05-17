6b2beff514e59ae23ff15d61a6297aa90c96fecc
roshan-malviya <roshan19@navgurukul.org>
2021-05-17 14:52:01 +0530

    add cors

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ const connectDB=require('./config/db')
 
 const express = require('express');
 const path= require('path');
+const cors = require('cors')
+
+
+
 
 const app = express();
 
-
 connectDB()
+app.use(cors())
 
 // Init Middeleware
 
